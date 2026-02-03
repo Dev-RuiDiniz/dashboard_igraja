92992721f6d67568563bed23dfd536f950779dd1
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-02-03 16:05:36 -0300

    Add responsible age calculation to family

--- src/IgrejaSocial.Application/DTOs/FamiliaDto.cs
+++ src/IgrejaSocial.Application/DTOs/FamiliaDto.cs
@@ -6,6 +6,7 @@ namespace IgrejaSocial.Application.DTOs
         public string NomeResponsavel { get; set; }
         public string CpfResponsavel { get; set; }
         public string Endereco { get; set; }
+        public int? IdadeResponsavel { get; set; }
         public decimal RendaPerCapita { get; set; } // Valor calculado
         public bool IsVulneravel { get; set; }      // Valor calculado
         public int TotalIntegrantes { get; set; }
