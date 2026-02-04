aafc89c0e9adbb4e01162d78c24afa2b672b32cd
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-02-03 21:05:24 -0300

    Serve Blazor WebAssembly assets from API

--- src/IgrejaSocial.API/Program.cs
+++ src/IgrejaSocial.API/Program.cs
@@ -64,8 +64,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
